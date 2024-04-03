9728ad652a80c7c89fd6049237593596c2480b07
Костянтин Борисюк <KOSTIANTYN.BORYSIUK@lnu.edu.ua>
2024-04-03 19:38:43 +0300

    main

--- ConsoleApplication4/main.cpp
+++ ConsoleApplication4/main.cpp
@@ -4,7 +4,47 @@
 
 int main()
 {
+    int a[5] = { 2, 8, 9, 3, 1 };
+    Set<int> A(a, 5);
+    std::cout << "A = " << A << " has size " << A.size() << '\n';
 
+    for (int i = 0; i < A.size(); ++i) std::cout << ' ' << A.to_array()[i];
+    std::cout << '\n';
 
-	//test
+    int b[4] = { 5, 3, 2, 0 };
+    Set<int> B(b, 4);
+    std::cout << "B = " << B << " has size " << B.size() << '\n';
+
+    Set<int> C = A.set_union(B);
+    std::cout << "A+B = " << C << " has size " << C.size() << '\n';
+    A.remove(1);
+    std::cout << "A = " << A << " has size " << A.size() << '\n';
+    A.remove(0);
+    std::cout << "A = " << A << " has size " << A.size() << '\n';
+    A.remove(8);
+    std::cout << "A = " << A << " has size " << A.size() << '\n';
+
+    try
+    {
+        A.remove(10);
+    }
+    catch (const std::runtime_error& ex)
+    {
+        std::cout << ex.what() << '\n';
+    }
+
+    for (int i = 3; i < 37; i += 5) A.add(i);
+    std::cout << "A = " << A << " has size " << A.size() << '\n';
+
+    Set<int> P = A.intersect(B);
+    std::cout << "A*B = " << P << " has size " << P.size() << '\n';
+
+    Set<int> Q = A.difference(B.add(33));
+    std::cout << "A-B-{33} = " << Q << " has size " << Q.size() << '\n';
+
+    Q.clear();
+    std::cout << "Q = " << Q << " has size " << Q.size() << '\n';
+
+    return 0;
+
 }
