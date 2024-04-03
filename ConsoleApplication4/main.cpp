8c508a3d8f8922a54804808067979c0b487a6d3a
Владислав Барабалюк <vlad251520@gmail.com>
2024-04-03 19:21:12 +0300

    add files

--- ConsoleApplication4/main.cpp
+++ ConsoleApplication4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "functions.h"
 
 
 int main()
---
+++ UnitTestingPlural/UnitTestingPlural.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication4/functions.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestingPlural
+{
+	TEST_CLASS(UnitTestingPlural)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
